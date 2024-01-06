660c1e1a6e72c76413448695bdadc637ee85ae35
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2024-01-06 14:49:55 -0400

    Initial Setup

---
+++ script.js
@@ -0,0 +1,12 @@
+// Constants
+const startEl = document.getElementById("start");
+const stopEl = document.getElementById("stop");
+const resetEl = document.getElementById("reset");
+const timerEl = document.getElementById("timer");
+
+// Function Start Timer
+function startTimer() {
+  console.log("start");
+}
+// Event Listener
+startEl.addEventListener("click", startTimer);
